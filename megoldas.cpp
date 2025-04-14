482d233022971868e53a728ed402d9373e4fe8c1
Zsoldos Csenge Adél <88947821+PrismChelsea@users.noreply.github.com>
2025-04-14 22:34:31 +0200

    upp

---
+++ megoldas.cpp
@@ -0,0 +1,53 @@
+﻿/*
+Készítsék el az alábbi szoftver implementációját az következőek szerint:
+• A szoftver foglaljon le dinamikusan egy 100 elemű egész típusú előjel nélküli
+tömböt nulla kezdőértékkel! (1p)
+• A szoftver feltölti az előzőleg lefoglalt tömb elemeit véletlenszerű értékekkel!
+(1p)
+• A szoftver kérjen be a felhasználótól számpárokat 0 és 99 között egészen addig,
+amíg -1-et nem kap a felhasználótól bármelyik szám esetében! (2p)
+• A szoftver egy eljárást alkalmazva a beolvasás alatt kicseréli a tömb megfelelő
+elemeit 29%-os eséllyel cseréli ki a számpárokat indexként és értékként
+felhasználva! (2p)
+• A szoftver kiírja azokat a tömbelemek értékeit, melyek az ASCII szerint
+karaktereknek feleltethetőek meg! A kiírás során a szoftver átkonvertálja ezeket
+a számértékeket a táblázatban található karakterkódnak megfelelően (2p)
+• A szoftver külön karaktertömbbe kigyűjti a kiírható értékeket! (2p)
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+void feltolt(unsigned int* tomb, unsigned int index, unsigned int value) {
+	unsigned int rand_value = rand() % 100;
+	if (rand_value <29)	tomb[index] = value;
+}
+
+int main(int argc, char* argv[])
+{
+	unsigned int* tomb = (unsigned int*)calloc(100, sizeof(unsigned int));
+	char karakterek[100];
+
+	unsigned int value1 = 0, value2 = 0;
+	while (value1 !=-1 && value2 !=-1) {
+		std::cin >> value1 >> value2;
+		if (value1 >= 0 && value1 <= 99 &&
+			value2 >= 0 && value2 <= 99)
+			feltolt(tomb, value1, value2);
+
+	}
+
+	int karakterindex = 0;
+	for (int i = 0; i < 100; i++)
+	{
+		if (tomb[i] >= 33 && tomb[i] <= 99) {
+			karakterek[karakterindex] = tomb[i];
+			std::cout << karakterek[karakterindex];
+			++karakterindex;
+		}
+	}
+	std::cout << std::endl;
+
+	free(tomb);
+	return 0;
+}
---
+++ megoldas_2.cpp
@@ -0,0 +1,51 @@
+﻿/*
+Az alapfeladat egy számkitalálós játék implementálása lesz. A játék szabályai az
+alábbiak lesznek:
+• A számítógép 20 számot fog véletlenszerűen generálni 1 és 100 között
+• A felhasználó ezekre fog tippet adni három körben. Ehhez a program be fog
+kérni 10 számot 3x, majd statisztikát készít, hogy körönként hányat talált el a
+felhasználó
+• A harmadik kör végén százalékos találati eredményeket ad vissza a program,
+hogy melyik számot a felhasználó hányszor találta el
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+	unsigned int szamok[20];
+	float tippek[20] = {}; //bool
+
+	for (int i = 0; i < 20; i++) {
+		szamok[i] = rand() % 100 + 1;
+	}
+	
+	for (int i = 0; i < 3; i++) {
+		float talalatok = 0;
+		bool talalt_szamok[20] = {};
+		std::cout << i << ".kör\n"; // \n ugyanaz mint az std::cout
+		for (int j = 0; j < 10; j++)
+		{
+			int tipp = 0;
+			std::cin >> tipp;
+			for (int k = 0; k < 20; k++) {
+				if (tipp == szamok[k] && !talalt_szamok[k]) {
+					tippek[k] += 1;
+					++talalatok;
+					talalt_szamok[k] = true;
+					break;
+				}
+			}
+		}
+		std::cout << "talalati arany: " << talalatok / 10;
+	}
+
+	for (int i = 0; i < 20; i++)
+	{
+		std::cout << szamok[i];
+		std::cout << " : " << 100*tippek[i] / 3;
+		std::cout << std::endl;
+	}
+	return 0;
+}
